e4ebd90867b3ef83641b3694f01b5b3d8c89f79b
Carlos Eduardo Egito <eduardo.egito@ifrn.edu.br>
2021-12-11 16:07:59 -0300

    Fixing the argument for security groups

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  aws_security_group = aws_security_group.allow_http
+  security_groups = [aws_security_group.allow_http]
   user_data = <<EOF
   #!/bin/bash
 
