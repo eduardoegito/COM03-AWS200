233f7a4c0cf0bce43b183ab0a91563586677c37f
Carlos Eduardo Egito <eduardo.egito@ifrn.edu.br>
2021-12-12 04:27:39 -0300

    Forget to put data in front of the name

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ data "aws_security_group" "default" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.default.name]
+  security_groups = [data.aws_security_group.default.name]
   user_data = <<EOF
   #!/bin/bash
 
