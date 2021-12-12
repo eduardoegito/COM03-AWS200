858d757cf23696f8cd0248ef5e9998be4cb79a0f
Carlos Eduardo Egito <eduardo.egito@ifrn.edu.br>
2021-12-12 12:24:23 -0300

    Allowing all traffic for testing

--- main.tf
+++ main.tf
@@ -62,8 +62,8 @@ resource "aws_security_group" "allow_http" {
 
   ingress {
     description      = "HTTP from Internet"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 0
+    to_port          = 0
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
