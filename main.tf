0c59b530994138f8152e9bc4e2f204131158552e
Carlos Eduardo Egito <eduardo.egito@ifrn.edu.br>
2021-12-12 13:03:44 -0300

    Adding key pair and new security rules

--- main.tf
+++ main.tf
@@ -40,7 +40,8 @@ data "aws_security_group" "default" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  security_groups = [data.aws_security_group.default.name]
+  security_groups = [aws_security_group.allow_http_ssh.name]
+  key_name = aws_key_pair.name
   user_data = <<EOF
   #!/bin/bash
 
@@ -55,15 +56,29 @@ EOF
   }
 }
 
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKsmf3/tJGZKJTzY2PLiqFQmp8g4UNFyvUrVFXztXWxvFl/kNsp4+7wO64hSzp/RGPyOHNi+7LkRS8SLQxnuvmh8EKKqHuqApb9RAdCGJOChXJON4J9N/Y+2ufAWDwYIj6DPdhe2Zz8caluqCGlB6zvLjouQx3etJhpSwn6f0gxE8UuhJuezEl+K+hW6BrMBpueJYibgEgRi17WftDRbOaLJ99zKvIz5q89MIaE/ckdC5Z26DCoNM7GYzMe1Vnnk/MoRGJE3oH11lYvzCzT4VEtYbo/funxk1Y7a2UV/KolTgUX4sRSTi8kTGaizIHHoLcRkek0Ea66YZs1gk5OFKF eduardoegito@re437505"
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
     description      = "HTTP from Internet"
-    from_port        = 0
-    to_port          = 0
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH from Internet"
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
