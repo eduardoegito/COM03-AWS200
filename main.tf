49b355419cbe52af918cbc24485cb81de95ebed1
Carlos Eduardo Egito <eduardo.egito@ifrn.edu.br>
2021-12-12 07:04:40 -0300

    Adjusting security group

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_security_groups" "default" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.allow_http.name]
+  security_groups = [aws_security_group.default.name]
   user_data = <<EOF
   #!/bin/bash
 
